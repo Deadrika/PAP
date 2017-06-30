9a903660408758207b901d514ff1337c6b0655ae
Deadrika <franciscoalmeida9418@gmail.com>
2017-06-30 13:17:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+ var config = {
+    apiKey: "AIzaSyBe6mm2Mx3l2BcRBgydK3FCtJWseNhRxfU",
+    authDomain: "ink-ster.firebaseapp.com",
+    databaseURL: "https://ink-ster.firebaseio.com",
+    projectId: "ink-ster",
+    storageBucket: "ink-ster.appspot.com",
+    messagingSenderId: "221665370008"
+  };
+  firebase.initializeApp(config);
+  
+  var provider = new firebase.auth.GoogleAuthProvider();
+  
+function google() { 
+ firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user);
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
