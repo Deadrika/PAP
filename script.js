aabe04b18f3e062cdd74e6fe2649eae4b2b914e3
Deadrika <franciscoalmeida9418@gmail.com>
2017-07-07 10:02:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- var config = {
+  var config = {
     apiKey: "AIzaSyBe6mm2Mx3l2BcRBgydK3FCtJWseNhRxfU",
     authDomain: "ink-ster.firebaseapp.com",
     databaseURL: "https://ink-ster.firebaseio.com",
@@ -16,7 +16,29 @@ function google() {
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
-  console.log(user);
+  console.log(user.val());
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
+
+var provider = new firebase.auth.FacebookAuthProvider();
+
+function facebook() {
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user.val());
   // ...
 }).catch(function(error) {
   // Handle Errors here.
